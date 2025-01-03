7c61d0d4922bb3d1c346bcd93985d6d33b2bf7ce
Aly Elhady <113694053+alyelhady9@users.noreply.github.com>
2025-01-03 20:43:33 +0200

    Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,15 @@
+"use client"
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const Footer = () => {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setYear(currentYear);
+  }, []);
+  
   return (
     <footer className=" bg-white text-gray-700  w-full ">
     <div className='bg-beige-600 w-8/12 max-lg:w-full mx-auto p-4 '> 
@@ -13,7 +22,7 @@ const Footer = () => {
            
         </div> */}
         <div className="container mx-auto text-center">
-            <p>© {new Date().getFullYear()} Visionary Interiors. All rights reserved.</p>
+            <p>© {year} Visionary Interiors. All rights reserved.</p>
         </div>
     </div>
     </footer>
